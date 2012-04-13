6567eb4d6993375457b4757a2617b5dccdfbae17
Dmitry Melnikov <dmitryme@gmail.com>
2012-04-13 17:04:21 +0400

    Update src/log_viewer.erl

--- src/log_viewer.erl
+++ src/log_viewer.erl
@@ -1,40 +1,37 @@
-%%
-%% %CopyrightBegin%
-%%
-%% Copyright Ericsson AB 1996-2011. All Rights Reserved.
-%%
-%% The contents of this file are subject to the Erlang Public License,
-%% Version 1.1, (the "License"); you may not use this file except in
-%% compliance with the License. You should have received a copy of the
-%% Erlang Public License along with this software. If not, it can be
-%% retrieved online at http://www.erlang.org/.
-%%
-%% Software distributed under the License is distributed on an "AS IS"
-%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
-%% the License for the specific language governing rights and limitations
-%% under the License.
-%%
-%% %CopyrightEnd%
-%%
 -module(log_viewer).
 
+-type rec_type() :: atom().
+-type filter() :: {'types', [rec_type()]} |
+                  {'reg_exp', string()} |
+                  {'interval', {calendar:datetime(), calendar:datetime()}} |
+                  {'interval', {calendar:datetime(), from}} |
+                  {'interval', {calendar:datetime(), to}}.
+-type filters() :: [filter()].
+-type list_result() :: list() | {'error', term()}.
+
 %% External exports
--export([list/0, list/2, rescan/1, show/1, get_types/0]).
+-export([list/0, list/1, rescan/1, show/1, get_types/0]).
 
+-spec list() -> list_result().
 list() ->
-   list([], all).
-list(RegExp, Types)
-      when is_list(RegExp) =/= true orelse (is_list(Types) == false andalso Types =/= all) ->
-   {error, wrong_args};
-list(RegExp, Types) ->
-   call({list, RegExp, Types}).
+   list([]).
 
+-spec list(filters()) -> list_result().
+list(Filter)
+      when is_list(Filter) =/= true ->
+   {error, wrong_args};
+list(Filter) ->
+   call({list, Filter}).
+
+-spec rescan(pos_integer()) -> filters() | {'error', term()}.
 rescan(Max) ->
    call({rescan, Max}).
 
+-spec show(all | pos_integer()) -> term().
 show(Number) when is_integer(Number) ->
    call({show_number, Number}).
 
+-spec get_types() -> filters() | {'error', term()}.
 get_types() ->
    call(get_types).
 
