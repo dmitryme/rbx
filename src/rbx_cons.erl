22b31b3af2c2aa0e6d6f76a2f9c368bb62bf93b9
Dmitry Melnikov <dmitryme@gmail.com>
2012-10-10 12:16:37 +0400

    Update src/rbx_cons.erl

--- src/rbx_cons.erl
+++ src/rbx_cons.erl
@@ -1,80 +1,54 @@
--module(rbx_cons).
+-module(rbx_inets).
 
 -behaviour(gen_server).
+
+-include_lib("inets/include/httpd.hrl").
 
 %% gen_server callbacks
 -export([start/0, start/1, start_link/1, init/1, terminate/2, handle_call/3,
          handle_cast/2, handle_info/2, code_change/3]).
 
-%% public exports
--export([list/0, list/1, rescan/1, show/0, show/1, start_log/1, stop_log/0, attach/1, detach/0]).
+-export([do/1]).
 
--record(state, {device}).
+-record(state, {document_root}).
 
-%======================================================================================================================
-%  gen_server interface functions
-%=======================================================================================================================
 start() -> start([]).
 start(Options) ->
     supervisor:start_child(sasl_sup,
-           	   {rbx_cons, {rbx_cons, start_link, [Options]},
-			    temporary, brutal_kill, worker, [rbx_cons]}).
+           	   {rbx_inets, {rbx_inets, start_link, [Options]},
+			    temporary, brutal_kill, worker, [rbx_inets]}).
 
 start_link(Options) ->
-   gen_server:start_link({local, rbx_cons}, ?MODULE, Options, []).
+   gen_server:start_link({local, rbx_inets}, ?MODULE, Options, []).
 
-list() ->
-   list([]).
-
-list(Filter) ->
-   gen_server:call(rbx_cons, {list, Filter}).
-
-rescan(MaxRecords) ->
-   gen_server:cast(rbx_cons, {rescan, MaxRecords}).
-
-show() ->
-   show(all).
-
-show(Number) ->
-   gen_server:call(rbx_cons, {show, Number}).
-
-start_log(Filename) ->
-   gen_server:cast(rbx_cons, {start_log, Filename}).
-
-stop_log() ->
-   gen_server:cast(rbx_cons, stop_log).
-
-attach(Node) -> ok.
-
-detach() -> ok.
-
-%=======================================================================================================================
-%  gen_server interface functions
-%=======================================================================================================================
 init(Options) ->
-   Log = rbx_utils:get_option(start_log, Options, standard_io),
-   Device = open_log_file(Log),
-   {ok, #state{device = Device}}.
+   inets:start(),
+   DocRoot = filename:nativename(rbx_utils:get_option(document_root, Options, ".")),
+   {ok, Pid} = inets:start(httpd, [
+      {port, rbx_utils:get_option(inets_port, Options, 8000)},
+      {server_name, "rbx"},
+      {server_root, "."},
+      {document_root, DocRoot},
+      {modules, [?MODULE]},
+      {mime_types, [{"css", "text/css"}, {"js", "text/javascript"}, {"html", "text/html"}]}
+   ]),
+   link(Pid),
+   {ok, #state{document_root = DocRoot}}.
 
-handle_call({list, Filters}, _From, State) ->
-   Reports = rbx:list(Filters),
-   print_list(State#state.device, true, Reports),
-   {reply, ok, State};
-handle_call({show, Number}, _From, State) ->
-   Res = record_formatter_cons:format(rbx:show(Number)),
-   {noreply, Res, State};
-handle_call(_, _, State) ->
-   {reply, ok, State}.
+handle_call(get_types, _, State) ->
+   {reply, rbx:get_types(), State};
+handle_call({get_records, Filters}, _From, State) ->
+   Records = rbx:list(Filters),
+   {reply, Records, State};
+handle_call({get_record, RecNum}, _From, State) ->
+   FmtRecord = record_formatter_html:format(rbx:show(RecNum)),
+   {reply, FmtRecord, State};
+handle_call(get_doc_root, _From, State) ->
+   {reply, State#state.document_root, State}.
 
 handle_cast({rescan, MaxRecords}, State) ->
    rbx:rescan(MaxRecords),
    {noreply, State};
-handle_cast({start_log, Filename}, State) ->
-   NewDevice = open_log_file(Filename),
-   {noreply, State#state{device = NewDevice}};
-handle_cast(stop_log, State) ->
-   close_device(State#state.device),
-   {noreply, State#state{device = standard_io}};
 handle_cast(_Msg, State) ->
    {noreply, State}.
 
@@ -87,45 +61,77 @@ handle_info(_Info, State) ->
 code_change(_OldVsn, State, _Extra) ->
    {ok, State}.
 
-%=======================================================================================================================
-%  private
-%=======================================================================================================================
-open_log_file(standard_io) -> standard_io;
-open_log_file(FileName) ->
-   case file:open(FileName, [write,append]) of
-      {ok, Fd} ->
-         Fd;
-	   Error ->
-	      io:format("rbx: Cannot open file '~s' (~w).~n", [FileName, Error]),
-	      io:format("rbx: Using standard_io~n"),
-         standard_io
+do(#mod{request_uri = Uri, entity_body = Query}) when Uri == "/rescan" ->
+   Response = rescan(Query),
+   {proceed, [{response, {200, Response}}]};
+do(#mod{request_uri = Uri, entity_body = Query}) when Uri == "/get_records" ->
+   Response = get_records(Query),
+   {proceed, [{response, {200, Response}}]};
+do(#mod{request_uri = Uri, entity_body = RecNum}) when Uri == "/get_record" ->
+   Response = get_record(list_to_integer(RecNum)),
+   {proceed, [{response, {200, Response}}]};
+do(#mod{request_uri = Uri})  when Uri == "/" ->
+   {ok, Bin} = file:read_file(get_doc_root() ++ "/www/index.html"),
+   Response = io_lib:format(binary_to_list(Bin), [node()]),
+   {proceed, [{response, {200, Response}}]};
+do(#mod{request_uri = Uri})  when Uri == "/favicon.ico" ->
+   {proceed, [{response, {404, ""}}]};
+do(#mod{request_uri = Uri}) ->
+   case file:read_file(get_doc_root() ++ Uri) of
+      {ok, Bin} ->
+         {proceed, [{response, {200, binary_to_list(Bin)}}]};
+      {error, Reason} ->
+         error_logger:error_msg("Unable to read file '~s'. Reason = ~p~n", [Uri, Reason]),
+         {proceed, [{response, {404, "ERROR: Page " ++ Uri ++ " not found."}}]}
    end.
 
-close_device(Fd) when is_pid(Fd) ->
-   catch file:close(Fd);
-close_device(_) -> ok.
 
-print_list(Device, UseSaslUtc, Reports) ->
-   Header = {"No", "Type", "Process", "Timestamp"},
-   PidWidth = find_proc_width([Header | Reports], 0) + 1,
-   Format = lists:concat(["~6s ~20s ~", PidWidth, "s ~20s~n"]),
-   io:format(Device, Format, tuple_to_list(Header)),
-   Format1 = lists:concat(["~6..=s ~20..=s ~", PidWidth, "..=s ~20..=s~n"]),
-   io:format(Device, Format1, ["", "", "", ""]),
-   print_list(Device, UseSaslUtc, Reports, PidWidth).
-print_list(_, _, [], _) -> true;
-print_list(Device, UseSaslUtc, [H|T], PidWidth) ->
-   print_one_report(Device, UseSaslUtc, H, PidWidth),
-   print_list(Device, UseSaslUtc, T, PidWidth).
+%%=============================================================
+%% utils
+%%=============================================================
 
-find_proc_width([], Width) -> Width;
-find_proc_width([{_No, _RepType, Pid, _Date}|T], Width) ->
-   Try = length(Pid),
-   if
-	   Try > Width -> find_proc_width(T, Try);
-	   true -> find_proc_width(T, Width)
-   end.
+get_doc_root() ->
+   gen_server:call(rbx_inets, get_doc_root).
 
-print_one_report(Device, UseSaslUtc, {No, RepType, Pid, Date}, PidWidth) ->
-   Format = lists:concat(["~6w ~20w ~", PidWidth, "s ~20s~n"]),
-   io:format(Device, Format, [No, RepType, Pid, rbx_utils:date_to_str(Date, UseSaslUtc)]).
+rescan(Query) when is_list(Query) ->
+   {ok, Tokens, _} = erl_scan:string(Query),
+   {ok, Term} = erl_parse:parse_term(Tokens),
+   rescan(Term);
+rescan({MaxRecords, RecOnPage, Filters}) ->
+   gen_server:cast(rbx_inets, {rescan, MaxRecords}),
+   get_records({Filters, 1, RecOnPage}).
+
+get_records(Query) when is_list(Query) ->
+   {ok, Tokens, _} = erl_scan:string(Query),
+   {ok, Term} = erl_parse:parse_term(Tokens),
+   get_records(Term);
+get_records({Filters, Page, RecOnPage}) ->
+   AllTypes = gen_server:call(rbx_inets, get_types),
+   Records = gen_server:call(rbx_inets, {get_records, Filters}),
+   lists:concat(["{\"types\":", list_to_json(AllTypes, fun(T) -> "\"" ++ atom_to_list(T) ++ "\"" end), ',',
+                  "\"records_count\":", length(Records), ',',
+                 "\"records\":", get_records(Records, Page, RecOnPage), '}']).
+get_records(Records, Page, RecOnPage) ->
+   StartFrom = lists:nthtail((Page - 1) * RecOnPage, Records),
+   PageRecords = lists:sublist(StartFrom, min(length(StartFrom), RecOnPage)),
+   list_to_json(PageRecords, fun record_to_json/1).
+
+get_record(RecNum) ->
+   gen_server:call(rbx_inets, {get_record, RecNum}).
+
+record_to_json({No, RepType, Pid, Date}) ->
+   lists:concat(["{\"no\":\"", No, "\",",
+   "\"type\":\"", RepType, "\",",
+   "\"pid\":\"", Pid, "\",",
+   "\"date\":\"", rbx_utils:date_to_str(Date, true), "\"}"]).
+
+list_to_json(List, Fun) ->
+   list_to_json(List, Fun, "[").
+list_to_json([], _Fun, Acc) ->
+   Acc ++ "]";
+list_to_json([Last], Fun, "[") ->
+   lists:concat(["[", Fun(Last), "]"]);
+list_to_json([Last], Fun, Acc) ->
+   lists:concat([Acc, Fun(Last), "]"]);
+list_to_json([H|T], Fun, Acc) ->
+   list_to_json(T, Fun, lists:concat([Acc, Fun(H), ","])).
